fda5a7034a2d39c62486df6e82f3ad62171a25f1
Void Main <voidmain1313113@gmail.com>
2012-08-31 08:29:28 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '71c7b5df01a1fe7c8b0b3afe782ce1620b8c8d4cd999ab6d3f72420448ca33387b0c8a9c61bf81a55b62152e5d25051a45331181db73d71f01a2438f52c49699'
